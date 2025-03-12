b4a3c1419107c5bb5aa66f87f6ddeaaba6cb70c7
Mohankumaram1 <132732401+Mohankumaram1@users.noreply.github.com>
2025-03-12 21:35:56 +0530

    Update provider.tf

--- my-serverfiles/provider.tf
+++ my-serverfiles/provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
 region     = "ap-south-1" 
+ access_key = "AKIASE5KRIECXJ6M6PKP"  # Replace with your AWS access key
+ secret_key = "sQoGK5pyDQyTVHXsMlO9AyEep4eVfFsF2ds4t/1o"  # Replace with your AWS secret key
 }
