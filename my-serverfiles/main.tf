55543544038e9f55963e2163412012210f19cf5f
Mohankumaram1 <132732401+Mohankumaram1@users.noreply.github.com>
2025-03-13 10:29:42 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,8 +1,12 @@
+ data "aws_vpc" "existing_vpc" {
+      id = "vpc-07a0f675711f3e439"
+    }
+
 resource "aws_instance" "test-server" {
   ami           = "ami-00bb6a80f01f03502" 
   instance_type = "t2.micro" 
   key_name      = "mohanm.pem"
-  subnet_id     = var.subnet-015b916651629df59
+  subnet_id     = "subnet-015b916651629df59"
   vpc_security_group_ids = ["sg-027a44025af40f8db"]
 
   connection {
