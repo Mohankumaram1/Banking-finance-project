c546213d0a86d73130e06a06535c351c1c2378c2
Mohankumaram1 <132732401+Mohankumaram1@users.noreply.github.com>
2025-03-14 10:45:36 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ansible_node" {
   instance_type          = "t2.micro"
   key_name               = "mohanm"
   subnet_id              = "subnet-04441ad5ed7050ca2"  # Replace with a valid subnet ID
-  vpc_security_group_ids = ["sg-027a44025af40f8db"]
+  vpc_security_group_ids = ["sg-026ff8db2ec83f7a0"]
   associate_public_ip_address = true   # Ensures a public IP is assigned
 
   connection {
